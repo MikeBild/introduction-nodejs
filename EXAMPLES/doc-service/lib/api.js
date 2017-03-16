ef9674057a4eb237ee819fe9482d41404e714f60
Mike Bild <mike.bild@googlemail.com>
2017-03-16 18:05:05 +0100

    rate limit

--- EXAMPLES/doc-service/lib/api.js
+++ EXAMPLES/doc-service/lib/api.js
@@ -2,10 +2,19 @@ const fs = require('fs');
 const https = require('https');
 const express = require('express');
 const expressBodyParser = require('body-parser');
+const RateLimit = require('express-rate-limit');
 const expressAuth = require('./auth-middleware');
 const docsApi = require('../routes/docs');
 const app = express();
 
+const limiter = new RateLimit({
+  windowMs: 15*60*1000,
+  max: 10,
+  delayMs: 0,
+  headers: true, 
+});
+
+app.use(limiter);
 app.use(expressBodyParser.json());
 app.use((req, res, next) => {
   const dbValues = {
