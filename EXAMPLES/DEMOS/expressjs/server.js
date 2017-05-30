ebedacba70d5910e82aaa22fef15a3c9e7cb75c2
Mike Bild <mike.bild@googlemail.com>
2017-05-30 08:59:58 +0200

    Enable route

--- EXAMPLES/DEMOS/expressjs/server.js
+++ EXAMPLES/DEMOS/expressjs/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 let instance = null;
 
-// app.get('/', (req, res) => res.send({}));
+app.get('/', (req, res) => res.send({}));
 
 module.exports = {
   start,
