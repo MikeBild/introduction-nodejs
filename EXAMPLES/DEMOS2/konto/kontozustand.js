1f088e0fc73028420719695f53293a1b441f6846
Mike Bild <mike.bild@googlemail.com>
2017-06-08 17:15:54 +0200

    more tests

---
+++ EXAMPLES/DEMOS2/konto/kontozustand.js
@@ -0,0 +1,12 @@
+module.exports = {
+  load,
+  save,
+};
+
+function load() {
+
+}
+
+function save() {
+
+}
--- EXAMPLES/DEMOS2/konto/test/konto.test.js
+++ EXAMPLES/DEMOS2/konto/test/konto.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
-const sut = require('../konto');
 
 describe('Konto tests', () => {
+  const sut = require('../konto');
+
   it('balance should yield 100 as initial value', () => {
     // Act
     const actual = sut.balance();
@@ -10,13 +11,27 @@ describe('Konto tests', () => {
     assert.equal(actual, 100);
   });
 
-   it('withdraw should descease balance', () => {
+  it('withdraw should decrease balance', () => {
 
     // Act
     const actual = sut.withdraw(80, 10);
 
-
     // Assert(s)
     assert.equal(actual, 70);
+  });
+
+});
+
+describe('Kontostandablage tests', () => {
+  const sut = require('../kontozustand');
+
+  it('load should yield a value', () => {
+    // Act
+    return sut.load('123')
+    .then(actual => {
+      // Assert(s)
+      assert.equal(actual, 70);
+    });
+
   });
 })
