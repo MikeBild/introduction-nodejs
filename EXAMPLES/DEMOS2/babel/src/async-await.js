66eeb5e43ab78a08c949b1a2560295f4c78e6590
Mike Bild <mike.bild@googlemail.com>
2017-06-09 16:42:47 +0200

    map reduce

--- EXAMPLES/DEMOS2/babel/src/async-await.js
+++ EXAMPLES/DEMOS2/babel/src/async-await.js
@@ -1,5 +1,6 @@
 import {readFile} from 'fs';
-import {Foo} from './classes';
+import Foo, {Bar} from './classes';
+import {withdraw, balance, add} from './konto-transactions'
 
 function load({path}) {
   return new Promise((resolve, reject) => {
@@ -13,15 +14,67 @@ function load({path}) {
   });
 }
 
+function hello(a, b, ...values) {
+  console.log(a, b, Array.prototype.slice.call(arguments), values)
+}
+
 (async () => {
-  const f1 = new Foo();
-  console.log(f1)
+  // hello('A', 'B', 'C', 'D')
+
+  const a = {
+    foo: 'bar',
+    a: '1',
+  }
+
+  const b = {
+    bar: 'foo',
+    a: '2',
+  }
+
+  const all = {
+    all: 'foo + bar',
+    ...a,
+    ...b,
+  }
+  // console.log(all)
+
+const myArray = ['shoulders', 'knees']
+const myNewArray = ['head', ...myArray, 'and', 'toes']
+// console.log(myNewArray)
+
+const allKeys = Object.keys(all).map(x => all[x])
+// console.log(allKeys)
+
+// ['A', 'B', 'C'].forEach(x => console.log(x))
+
+const results = [1, 2, 3, 4]
+  .filter(x => x % 2 === 0)
+  .map((x, i) => {
+    return x + x + i
+  })
+  .reduce((state, element) => {
+    state += element;
+    return state;
+  }, 0);
+
+// console.log(results)
+
+  withdraw(30);
+  withdraw(20);
+  add(60);
+  console.log(balance());
+
+  Foo.bar()
+  const f1 = new Foo('1234');
+  f1.foo()
+  const b1 = new Bar();
+  // console.log(b1, f1)
 
   try {
     const {fileContent} = await load({path: '123.txt'});
     const gg = await load({path: '123.txt'});
 
-    console.log(fileContent);
+    // console.log(fileContent);
   } catch(error) {
     console.error(error);
   }
--- EXAMPLES/DEMOS2/babel/src/classes.js
+++ EXAMPLES/DEMOS2/babel/src/classes.js
@@ -1,5 +1,21 @@
-export class Foo {
+export class Bar {
   constructor() {
-    console.log('constructor')
+    // console.log('bar constructor')
+  }
+}
+
+export default class Foo extends Bar {
+  static bar () {
+    // console.log('bar bar')
+  }
+
+  constructor(bla) {
+    super()
+    this.bla = bla
+    // console.log('foo constructor')
+  }
+
+  foo () {
+    // console.log('just do it!')
   }
 }
