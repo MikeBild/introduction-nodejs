6184fa466e91cdc74062e80593b8a373b3962a5e
Mike Bild <mike.bild@googlemail.com>
2017-11-06 17:22:27 +0100

    more test

--- EXAMPLES/HTTP_SERVER_DEMO/lib/foo.js
+++ EXAMPLES/HTTP_SERVER_DEMO/lib/foo.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
+const util = require("util");
 
 module.exports.sumSync = values =>
   values.reduce((state, value) => (state += value), 0);
 
-module.exports.sum = (filePath, callback) => {
-  fs.readFile(filePath, (err, data) => {
-    if (err) return callback(err);
+module.exports.sum = filePath => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) return reject(err);
 
-    const dataAsObj = JSON.parse(data.toString());
-    const result = module.exports.sumSync(dataAsObj);
+      const dataAsObj = JSON.parse(data.toString());
+      const result = module.exports.sumSync(dataAsObj);
 
-    callback(null, result);
+      resolve(result);
+    });
   });
 };
+
+module.exports.sumPromisified = filePath =>
+  util.promisify(fs.readFile)(filePath).then(data => {
+    const dataAsObj = JSON.parse(data.toString());
+    return module.exports.sumSync(dataAsObj);
+  });
--- EXAMPLES/HTTP_SERVER_DEMO/test/foo.test.js
+++ EXAMPLES/HTTP_SERVER_DEMO/test/foo.test.js
@@ -9,14 +9,54 @@ describe("Some foo tests ...", () => {
   });
 });
 
-describe("Some foo integration tests ...", () => {
-  it("read file and sum should be 6", done => {
+describe("Some foo integration tests ...", function() {
+  this.timeout(10000);
+
+  it("should read file and sum should be 6", () => {
     const pathToFile = path.resolve("./test/foo.json");
 
-    foo.sum(pathToFile, (err, actual) => {
-      assert.equal(err, null);
+    return foo.sum(pathToFile).then(actual => {
       assert.equal(actual, 6);
-      done();
+    });
+  });
+
+  it("should read file and sumPromisified should be 6", () => {
+    const pathToFile = path.resolve("./test/foo.json");
+
+    return foo.sumPromisified(pathToFile).then(actual => {
+      assert.equal(actual, 6);
+    });
+  });
+
+  it("should read two files and sum should be 12", () => {
+    const pathToFile = path.resolve("./test/foo.json");
+
+    return foo
+      .sum(pathToFile)
+      .then(data => foo.sum(pathToFile).then(data2 => [data, data2]))
+      .then(data => foo.sumSync(data))
+      .then(actual => {
+        assert.equal(actual, 12);
+      });
+  });
+
+  it("should read files in parallel and sum should be 12", () => {
+    const pathToFile = path.resolve("./test/foo.json");
+    const pathToFile2 = path.resolve("./test/foo2.json");
+
+    return Promise.all([foo.sum(pathToFile), foo.sum(pathToFile2)])
+      .then(data => foo.sumSync(data))
+      .then(actual => {
+        assert.equal(actual, 12);
+      })
+      .catch(_ => Promise.resolve(33));
+  });
+
+  it("should throw an error file not exists", () => {
+    const pathToFile = path.resolve("./test/foo2.json");
+
+    return foo.sum(pathToFile).catch(actual => {
+      assert.notEqual(actual, null);
     });
   });
 });
