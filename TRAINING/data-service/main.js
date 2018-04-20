ab754349ef9df8eb77f6dab02e43cd89539cd7b3
Mike Bild <mike.bild@googlemail.com>
2018-04-20 15:09:32 +0200

    More data

--- TRAINING/data-service/main.js
+++ TRAINING/data-service/main.js
@@ -1,8 +1,15 @@
+const sqlite = require("sqlite");
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
-const server = require("./server")({ isAuthenticationEnabled: false });
+const server = require("./server");
+const dbPromise = sqlite.open("./data-server.db");
 
-const app = server.listen(8080, () => {
-  console.log(`Is in production: ${IS_PRODUCTION}`);
-  console.log(`Is auth enabled: ${server.config.isAuthenticationEnabled}`);
-  console.log(`Listen on ${app.address().port}`);
-});
+(async () => {
+  const db = await dbPromise;
+  const instance = server({ isAuthenticationEnabled: false, db });
+
+  const app = instance.listen(8080, () => {
+    console.log(`Is in production: ${IS_PRODUCTION}`);
+    console.log(`Is auth enabled: ${instance.config.isAuthenticationEnabled}`);
+    console.log(`Listen on ${app.address().port}`);
+  });
+})();
