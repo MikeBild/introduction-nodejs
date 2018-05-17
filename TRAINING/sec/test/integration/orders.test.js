d9630ff54bb326fc0be311c483a9054e5f5dbf7b
Mike Bild <mike.bild@googlemail.com>
2018-05-17 11:18:03 +0200

    refactor: to async/await

--- TRAINING/sec/test/integration/orders.test.js
+++ TRAINING/sec/test/integration/orders.test.js
@@ -12,29 +12,32 @@ describe("Order integration tests", () => {
     instance.close(() => done());
   });
 
-  it("GET /orders should return a 200 status code", () => {
-    return fetch(`http://localhost:${instance.address().port}/orders`).then(
-      response => {
-        equal(response.status, 200);
+  it("GET /orders should return a 200 status code", async () => {
+    const response = await fetch(
+      `http://localhost:${instance.address().port}/orders`
+    );
+    equal(response.status, 200);
+  });
+
+  it("GET /orders should return a list", async () => {
+    const response = await fetch(
+      `http://localhost:${instance.address().port}/orders`
+    );
+    const actual = await response.json();
+    equal(Array.isArray(actual), true);
+  });
+
+  it("POST /orders should return 201 status code", async () => {
+    const response = await fetch(
+      `http://localhost:${instance.address().port}/orders`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({})
       }
     );
-  });
-
-  it("GET /orders should return a list", () => {
-    return fetch(`http://localhost:${instance.address().port}/orders`)
-      .then(response => response.json())
-      .then(actual => {
-        equal(Array.isArray(actual), true);
-      });
-  });
-
-  it("POST /orders should return 201 status code", () => {
-    return fetch(`http://localhost:${instance.address().port}/orders`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({})
-    }).then(response => equal(response.status, 201));
+    equal(response.status, 201);
   });
 });
