d383d58b15e35d3ec4662f54fb3216a98abe4efe
Mike Bild <mike.bild@googlemail.com>
2018-05-17 15:03:01 +0200

    feature: production ready!

--- TRAINING/sec/index.js
+++ TRAINING/sec/index.js
@@ -1,6 +1,23 @@
+const PORT = process.env.PORT;
+const ENV = process.env.NODE_ENV || "development";
 const server = require("./server");
+const config = require("./package.json");
 
 (async () => {
-  const instance = await server({ port: 8080 });
+  const instance = await server({ port: PORT || config.sec[ENV].port });
   console.log(`Listen on ${instance.address().port}`);
+
+  //Graceful shutdown!
+  process.on("SIGTERM", () => {
+    process.exit(0);
+  });
+
+  process.on("SIGINT", () => {
+    console.log("Shutdown initiated!");
+    //Cleanup and close all resources!!!
+    instance.close(() => {
+      console.log("Shutdown!");
+      process.exit(0);
+    });
+  });
 })();
