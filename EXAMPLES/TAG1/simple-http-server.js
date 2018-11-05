e0b1cdbbc726420e8519aeeea9851e98862df3ad
Mike Bild <mike.bild@googlemail.com>
2018-11-05 15:03:24 +0100

    more

--- EXAMPLES/TAG1/simple-http-server.js
+++ EXAMPLES/TAG1/simple-http-server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 8080;
 const { createServer } = require('http');
 
 const server = createServer((req, res) => {
@@ -5,6 +6,17 @@ const server = createServer((req, res) => {
   res.end();
 });
 
-const instance = server.listen(8080, () => {
+const instance = server.listen(PORT, () => {
   console.log(`Listen on ${instance.address().port}`);
 });
+
+process.on('SIGTERM', onShutdown);
+process.on('SIGINT', onShutdown);
+
+function onShutdown() {
+  console.log('Shutting down ... ');
+  instance.close(() => {
+    process.stdout.write('Done ...');
+    process.exit(0);
+  });
+}
