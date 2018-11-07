1816798ba50fee7f2cf7a93619b8886f1c79de91
Mike Bild <mike.bild@googlemail.com>
2018-11-07 10:53:27 +0100

    more

--- EXAMPLES/atomics-template/index.js
+++ EXAMPLES/atomics-template/index.js
@@ -1,9 +1,13 @@
 const { join } = require('path');
 const { wafiosLogger } = require('wafios-logger');
+const { WafiosMetrics } = require('wafios-metrics');
+const { CreateServiceBus } = require('service-bus');
 
 const PROJECTPKG = require(join(process.cwd(), 'package.json'));
 
 module.exports = {
   pkg: PROJECTPKG,
   logger: wafiosLogger(`${PROJECTPKG.name}:${PROJECTPKG.version}`),
+  metrics: new WafiosMetrics({}),
+  servicebus: new CreateServiceBus({ db: '0', host: 'localhost', port: 6379 }),
 };
