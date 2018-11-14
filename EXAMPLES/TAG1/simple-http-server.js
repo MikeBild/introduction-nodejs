e2035147b98a07e54748d8ebb4daaf8f6d7c7254
Mike Bild <mike.bild@googlemail.com>
2018-11-14 13:46:16 +0100

    run a simple HTTP server

--- EXAMPLES/TAG1/simple-http-server.js
+++ EXAMPLES/TAG1/simple-http-server.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+const http = require('http');
+
+const server = http.createServer((request, respose) => {
+  const method = request.method;
+
+  respose.write(`Hello World via ${method}`);
+  respose.end();
+});
+
+server.listen(8080, () => console.log(`Listen on 8080`));
