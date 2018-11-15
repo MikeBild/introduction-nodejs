366c626b90532cec19068c2aa8c213f01e56c7c5
Mike Bild <mike.bild@googlemail.com>
2018-11-15 17:04:24 +0100

    more

--- EXAMPLES/billing-machine/express.js
+++ EXAMPLES/billing-machine/express.js
@@ -5,6 +5,17 @@ const registerRoutes = require('./routes/register');
 const storeMiddleware = require('./storeMiddleware');
 
 const consumers = [{ id: 'mike' }, { id: 'peter' }];
+const products = [{ id: 'lecker', price: 0.25 }, { id: 'groß', price: 0.5 }];
+const billings = {
+  mike: ['fruchtig', 'lecker', 'groß', 'lecker'],
+  peter: ['fruchtig', 'lecker'],
+  hans: ['fruchtig', 'fruchtig'],
+};
+
+Object.keys(billings).map(key => {
+  const matchedConsumer = consumers.find(consumer => consumer.id === key);
+  return matchedConsumer;
+});
 
 app.set('view engine', 'ejs');
 
