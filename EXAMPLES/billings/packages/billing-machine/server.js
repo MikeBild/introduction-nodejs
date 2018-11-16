256a4c6164da793e0985e5f0f668ab0aeae4904f
Mike Bild <mike.bild@googlemail.com>
2018-11-16 17:07:40 +0100

    bester

--- EXAMPLES/billings/packages/billing-machine/server.js
+++ EXAMPLES/billings/packages/billing-machine/server.js
@@ -1,22 +1,41 @@
-const server = require('./express');
+// require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+const DB = process.env.DB || 'database.json';
+
+const { start, stop } = require('./express');
+const { readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+
+const readFilePromise = promisify(readFile);
+const writeFilePromise = promisify(writeFile);
 
 main();
 
+let instance = null;
+let store = null;
+
 async function main() {
-  const consumers = [{ id: 'mike' }, { id: 'peter' }];
-  const products = [{ id: 'lecker', price: 0.25 }, { id: 'groß', price: 0.5 }];
-  const billings = {
-    mike: ['fruchtig', 'lecker', 'groß', 'lecker'],
-    peter: ['fruchtig', 'lecker'],
-    hans: ['fruchtig', 'fruchtig'],
-  };
-
-  const store = {
-    consumers,
-    products,
-    billings,
-  };
-
-  const instance = await server.start({ port: 8080, store });
+  store = JSON.parse(await readFilePromise(DB));
+  instance = await start({ port: PORT, store });
   console.log(`Listen on ${instance.address().port}`);
+}
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
+
+async function gracefulShutdown() {
+  console.log('Graceful shutdown .... ');
+  try {
+    console.log('Write  Database');
+    await writeFilePromise(DB, JSON.stringify(store, null, 2));
+    console.log('Stop Express');
+    await stop(instance);
+    console.log('Graceful stopped .... ');
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
