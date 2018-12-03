731427d4444535d910f799f3e79b02e0c4b12d6a
Mike Bild <mike.bild@googlemail.com>
2018-12-03 16:56:12 +0100

    docs: Promise.all

--- EXAMPLES/TAG1/promises.js
+++ EXAMPLES/TAG1/promises.js
@@ -1,5 +1,11 @@
 const { doSomething: doSomethingCallback } = require('./callbacks');
 
+const { readFile: readFileCallback } = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(readFileCallback);
+// Node >= 10
+// const { readFile } = require('fs').promises;
+
 module.exports = {
   doSomething,
 };
@@ -21,3 +27,25 @@ const p1 = doSomething('Bar')
   .catch(error => console.error(error));
 
 console.log(p1);
+
+// Parallel
+const simpleFileList = Promise.all([readFile('a.json'), readFile('b.json')])
+  .then(dataList => {
+    console.log(dataList.map(data => data.toString()));
+  })
+  .catch(error => console.error(error));
+
+const fileList = ['a.json', 'b.json'];
+const readFileList = fileList.map(fileName =>
+  readFileWithDefaultValue(fileName)
+);
+
+Promise.all(readFileList)
+  .then(dataList => {
+    console.log(dataList.map(data => data.toString()));
+  })
+  .catch(error => console.error(error));
+
+function readFileWithDefaultValue(fileName, defaultValue = '{}') {
+  return readFile(fileName).catch(error => defaultValue);
+}
