8d64a0f429034e3e31e3a81693da9db9a5501a27
Mike Bild <mike.bild@googlemail.com>
2018-12-04 17:27:45 +0100

    refactor: code cleanup

--- EXAMPLES/dasamt/antragsbestaetigung/routes/antraege.js
+++ EXAMPLES/dasamt/antragsbestaetigung/routes/antraege.js
@@ -1,18 +1,13 @@
 
-const { writeFile: writeFileCallback, readdir: readdirCallback, readFile: readFileCallback } = require('fs')
+const { readdir: readdirCallback } = require('fs')
 const { promisify } = require('util')
-const path = require('path');
-
 const readdir = promisify(readdirCallback)
-const readfile = promisify(readFileCallback)
-const writeFile = promisify(writeFileCallback)
-
 const express = require('express')
 const app = express.Router()
 
 module.exports = app
 
 app.get('/', async (req, res) => {
-  const dirlist = await readdir(req.config.DATA_FOLDER)
-  res.render('antraege', { title: 'Anträge', files: dirlist })
+  const files = await readdir(req.config.DATA_FOLDER)
+  res.render('antraege', { title: 'Anträge', files })
 })
--- EXAMPLES/dasamt/antragsbestaetigung/routes/antragsbestaetigung.js
+++ EXAMPLES/dasamt/antragsbestaetigung/routes/antragsbestaetigung.js
@@ -1,10 +1,9 @@
 
-const { writeFile: writeFileCallback, readdir: readdirCallback, readFile: readFileCallback } = require('fs')
+const { writeFile: writeFileCallback, readFile: readFileCallback } = require('fs')
 const { promisify } = require('util')
 const path = require('path');
 
-const readdir = promisify(readdirCallback)
-const readfile = promisify(readFileCallback)
+const readFile = promisify(readFileCallback)
 const writeFile = promisify(writeFileCallback)
 
 const express = require('express')
@@ -14,14 +13,14 @@ module.exports = app
 
 app.get('/:fileName', async (req, res) => {
   const { fileName } = req.params
-  const fileContent = await readfile(path.join(req.config.DATA_FOLDER, fileName));
+  const fileContent = await readFile(path.join(req.config.DATA_FOLDER, fileName));
   const urlaubsantrag = JSON.parse(fileContent)
   res.render('antragsbestaetigung', { title: 'antragsbestaetigung', urlaubsantrag, fileName })
 })
 
 app.post('/:fileName/:ok', async (req, res) => {
   const { fileName, ok } = req.params;
-  const fileContent = await readfile(path.join(req.config.DATA_FOLDER, fileName));
+  const fileContent = await readFile(path.join(req.config.DATA_FOLDER, fileName));
   const urlaubsantrag = JSON.parse(fileContent);
   const neuerUrlaubsantrag = { ...urlaubsantrag, ok: ok === 'yes' ? true : false };
 
