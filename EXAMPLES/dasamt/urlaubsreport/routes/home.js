d01a664752e9ee7f735119d9cdd9977eda9ebe0e
Mike Bild <mike.bild@googlemail.com>
2018-12-05 13:07:09 +0100

    refactor: generate Urlaubsreport view

--- EXAMPLES/dasamt/urlaubsreport/routes/home.js
+++ EXAMPLES/dasamt/urlaubsreport/routes/home.js
@@ -3,34 +3,35 @@ const { readdir: readdircallback } = require('fs');
 const readdir = promisify(readdircallback);
 const { readFile: readFileCallback } = require('fs');
 const readFile = promisify(readFileCallback);
-const express = require('express')
-const app = express.Router()
+const generateUrlaubsreport = require('../lib/generateUrlaubsreport');
+const express = require('express');
+const app = express.Router();
 
-module.exports = app
+module.exports = app;
 
 app.get('/', async (req, res, next) => {
-  try {
-    const fileNameList = await readdir('data')
-    const readFileListAsPromise = fileNameList.map(fileName =>
-      readFileWithDefaultValue('data/' + fileName)
-    )
-    const fileContentList = await Promise.all(readFileListAsPromise)
-    const urlaubsReporte = fileContentList
-      .map(fileContent => JSON.parse(fileContent))
-      .map(urlaubsAntragBestaetigung => ({ ...urlaubsAntragBestaetigung, rest: calculateRest(urlaubsAntragBestaetigung) }));
+	try {
+		const fileNameList = await readdir('data');
+		const readFileListAsPromise = fileNameList.map((fileName) =>
+			readFileWithDefaultValue('data/' + fileName)
+		);
+		const fileContentList = await Promise.all(readFileListAsPromise);
+		const urlaubsAntraege = fileContentList.map((fileContent) =>
+			JSON.parse(fileContent)
+		);
+		const urlaubsReport = generateUrlaubsreport(urlaubsAntraege, 30);
+		const urlaubsReporte = Object.values(urlaubsReport);
 
-    res.render('home', { title: 'Urlaubsreport', urlaubsReporte })
-  }
-  catch (error) {
-    next(error)
-  };
-})
+		res.render('home', { title: 'Urlaubsreport', urlaubsReporte });
+	} catch (error) {
+		next(error);
+	}
+});
 
 function readFileWithDefaultValue(fileName, defaultValue = '{}') {
-  return readFile(fileName).catch(error => defaultValue);
+	return readFile(fileName).catch((error) => defaultValue);
 }
 
 function calculateRest({ amount, anzahlProJahr = 30 }) {
-  return anzahlProJahr - amount;
+	return anzahlProJahr - amount;
 }
-
