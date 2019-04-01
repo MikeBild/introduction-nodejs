5df7779696438f4696c57bf956dac5a15d80ff6a
Mike Bild <mike.bild@googlemail.com>
2019-04-01 17:29:56 +0200

    chore: first impl

---
+++ EXAMPLES/LOGIN-APP/src/index.js
@@ -0,0 +1,4 @@
+import { render as renderRegister } from './register';
+
+const rootElement = document.getElementById('root');
+rootElement.innerHTML = renderRegister();
---
+++ EXAMPLES/LOGIN-APP/src/register.js
@@ -0,0 +1,9 @@
+export function render() {
+  return `
+  <div>
+    <h1>Register</h1>
+  </div>
+  `;
+}
+
+export default render;
