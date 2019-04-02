c4b0fa6e6ca62e8f0a489f59fa90e45d3746fee5
Mike Bild <mike.bild@googlemail.com>
2019-04-02 10:52:17 +0200

    feat: add register form

--- EXAMPLES/LOGIN-APP/src/register.js
+++ EXAMPLES/LOGIN-APP/src/register.js
@@ -3,6 +3,13 @@ export function render() {
   return `
   <div id="register">
     <h1>Register ...</h1>
+    <fieldset>
+      <input type="text" id="username" placeholder="username" />
+      <input type="password" id="password" placeholder="password" />
+      <input type="password" id="verify" placeholder="verify password" />
+      <input type="date" id="birthdate" placeholder="date of birth" />
+      <button onclick="registerUser()">Register</button>
+    </fieldset>
     <button onclick="goToLogin()">Go to login</button>
   </div>
   `;
@@ -11,4 +18,13 @@ export function render() {
 window.goToLogin = () => {
   $('#register').hide();
   $('#login').show();
+};
+
+window.registerUser = () => {
+  const username = $('#username').val();
+  const password = $('#password').val();
+  const verify = $('#verify').val();
+  const birthdate = $('#birthdate').val();
+
+  console.log({ username, password, verify, birthdate });
 };
