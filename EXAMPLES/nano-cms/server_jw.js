82e04b1daaa3caa02d1698761189a6b82c57f69c
Jörg Weilandt <joerg.weilandt@axelspringer.com>
2019-09-11 13:08:46 +0200

    Jörg

---
+++ EXAMPLES/nano-cms/server_jw.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const instance = express();
+instance.use(express.json());
+
+const accountList = [];
+
+instance.get('/accounts', (req, res) => {
+    res.send(JSON.stringify(accountList))
+});
+
+instance.post('/accounts:account', (req, res) => {
+    const newAccount = req.account;
+    // Account anlegen
+});
+
+instance.listen(8080, () => {
+    console.log('Listen on 8080');
+});
