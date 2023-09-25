0d6cb954a0cf3804944a5bcb1ef5485f575b89d7
Mike Bild <mike.bild.ext@siemens.com>
2023-09-25 10:49:59 +0200

    env vars

---
+++ EXAMPLES/GEOSN/ASS/src/cli.ts
@@ -0,0 +1,29 @@
+// https://12factor.net/
+
+/*
+node build/src/cli.js -p 8080 -t bla.txt
+npm run cli -- -p 8080 -t bla.txt
+*/
+
+console.log(process.argv);
+/*
+[
+  '/Users/mikebild/.nvm/versions/node/v18.17.1/bin/node',
+  '/Users/mikebild/Projects/introduction-nodejs/EXAMPLES/GEOSN/ASS/build/src/cli.js',
+  '-p',
+  '8080',
+  '-t',
+  'bla.txt'
+]
+*/
+
+/*
+export PORT=8080 (windows)
+PORT=8080 node build/src/cli.js
+*/
+console.log(process.env.PORT);
+
+const [_, __, ___, paramPort, fileNameParam, fileName] = process.argv;
+const envPort = process.env.PORT;
+const usedPort = envPort || paramPort || "8181";
+console.log(`Hello World! ${usedPort}, ${fileName}`);
